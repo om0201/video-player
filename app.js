331e38c862b402ee7efea1b6e01a42ea76dfdc02
Omang Morekar <omangmorekar68@gmail.com>
2022-12-16 07:38:48 +0530

    find duration

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ const jsmediatags = window.jsmediatags;
 const userFile = document.querySelector("#select-file");
 const cover = document.querySelector(".cover");
 const track = document.querySelector(".current-music");
+const manBtn = document.querySelector(".pl-ps");
+
+manBtn.addEventListener("click", (e) => {
+  track.paused ? track.play() : track.pause();
+  // console.log(track);
+});
 
 userFile.addEventListener("change", (e) => {
   const file = e.target.files[0];
+
+  console.log(file);
 
   const url = URL.createObjectURL(file);
 
   track.src = url;
 
+  track.addEventListener("loadedmetadata", (e) => {
+    console.log(e);
+
+    // const minutes = track.duration / 60;
+
+    console.log(track.duration);
+  });
+
   jsmediatags.read(file, {
     onSuccess: function (tag) {
+      console.log(tag);
+
       const img = tag.tags.picture.data;
       const format = tag.tags.picture.format;
       let base64Str = "";
